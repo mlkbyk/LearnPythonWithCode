8a53c27745f4d4565dff4c6f07e4de37f27ad3bb
mlkbyk <166039308+mlkbyk@users.noreply.github.com>
2024-04-26 14:42:30 +0300

    1.printing.py
    Python 1
    How to print?

---
+++ 1.printing.py
@@ -0,0 +1,16 @@
+#HOW TO PRİNT IN THE PYTHON.
+
+#If you add hashtag '#' this line of code does no work.
+#When you want to print something you should use 'print' function with double quoete.
+print("Hello World!")
+
+#Printing the pasta ingredients.
+#When you add to new line you do not need to more tahn once print .You can use '\n'
+print("1 cup of sugar.\n1 tablespoon of vanilla.\n3.5 cup of flour.\n1 packet cocoa.\n150 grams of coconut oil.")
+
+#When you combine to different code use the plus '+' sign.
+print("I love "+"Maria")
+
+#IMPORTANT!!!!!!!
+#If we make a space before print 'indention error 'happens.
+#When I add unnecesarry open paranthesis codes does not work right.
